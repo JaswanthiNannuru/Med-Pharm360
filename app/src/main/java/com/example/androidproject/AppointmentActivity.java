bdddf7bbdeb2163ff8ce307111f24ea4eda887d9
HarshaVardhan Reddy Bollam <bollamharshavardhanreddy@gmail.com>
2019-10-04 21:03:51 -0500

    commited appointment

---
+++ app/src/main/java/com/example/androidproject/AppointmentActivity.java
@@ -0,0 +1,23 @@
+package com.example.androidproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AppointmentActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_doctordata);
+        }
+        public static final int appointment =  0;
+
+        public void conformClick(View v){
+            Intent intent = new Intent(this, AppCompatActivity.class);
+            startActivityForResult(intent,appointment);
+
+        }
+
+}
---
+++ app/src/main/java/com/example/androidproject/DoctorslistActivity.java
@@ -0,0 +1,22 @@
+package com.example.androidproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DoctorslistActivity extends AppointmentActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctorslist);
+    }
+    public static final int list =  0;
+
+    public void conformClick(View v){
+        Intent intent = new Intent(this, AppointmentActivity.class);
+        startActivityForResult(intent,list);
+
+    }
+}
