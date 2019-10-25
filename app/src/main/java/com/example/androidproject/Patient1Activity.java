2e81b9e48efb6f43904ab3b5aee8d5de7a1c0e33
sanjana baswapuram <baswasanjana@gmail.com>
2019-10-25 18:35:05 -0500

    committed

--- app/src/main/java/com/example/androidproject/Patient1Activity.java
+++ app/src/main/java/com/example/androidproject/Patient1Activity.java
@@ -3,6 +3,7 @@ package com.example.androidproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,8 +13,14 @@ public class Patient1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient1);
+
+        //TextView patient1 = findViewById(R.id.patient1reportTV);
+        //patient1.setText("Name: abcd"+ \n + "Age: 23");
+
     }
     public static final int patient1back = 0;
+
+
 
     public void patient1backClick(View v) {
         Intent intent = new Intent(this, HistoryloginActivity.class);
