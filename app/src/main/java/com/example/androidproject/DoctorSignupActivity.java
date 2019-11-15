a82499c87a3b9f62279a9827fea11960de35d02c
jaswanthi nannuru <jaswanthinannuru26242@gmail.com>
2019-11-14 21:09:42 -0600

    commit

--- app/src/main/java/com/example/androidproject/DoctorSignupActivity.java
+++ app/src/main/java/com/example/androidproject/DoctorSignupActivity.java
@@ -37,39 +37,6 @@ public class DoctorSignupActivity extends AppCompatActivity {
         final EditText doctorconformpassword = (EditText) findViewById(R.id.doctorconformpasswordET);
         Button doctorsignupbutton = findViewById(R.id.doctorsignupBTN);
 
-<<<<<<< .mine
-
-    final Button back_button = findViewById(R.id.back2BTN);
-        back_button.setOnClickListener(new View.OnClickListener()
-    {
-        @Override
-        public void onClick (View v){
-        final ProgressDialog dlg = new ProgressDialog(DoctorSignupActivity.this);
-        dlg.setTitle("Please, wait a moment.");
-        dlg.setMessage("Returning to the login section...");
-        dlg.show();
-        Intent intent = new Intent(DoctorSignupActivity.this, MainActivity.class);
-        dlg.dismiss();
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-    }
-=======
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> .theirs
 
         Button backButton2 = findViewById(R.id.back2BTN);
 
@@ -90,95 +57,95 @@ public class DoctorSignupActivity extends AppCompatActivity {
 
 
 
-    doctorsignupbutton.setOnClickListener(new View.OnClickListener() {
+        doctorsignupbutton.setOnClickListener(new View.OnClickListener() {
 
-        @Override
-        public void onClick (View v){
-        //Validating the log in data
-        boolean validationError = false;
-
-        StringBuilder validationErrorMessage = new StringBuilder("Please, insert ");
-        if (isEmpty(doctorusername)) {
-            validationError = true;
-            validationErrorMessage.append("username");
-        }
-        if (isEmpty(departmentname)) {
-            if (validationError) {
-                validationErrorMessage.append("department name");
-            }
-            validationError = true;
-            validationErrorMessage.append(" ");
-        }
-        if (isEmpty(doctormailid)) {
-            if (validationError) {
-                validationErrorMessage.append("doctor mailid");
-            }
-            validationError = true;
-            validationErrorMessage.append(" ");
-        }
-        if (isEmpty(doctorphno)) {
-            if (validationError) {
-                validationErrorMessage.append("doctor phonenumber");
-            }
-            validationError = true;
-            validationErrorMessage.append(" ");
-        }
-        if (isEmpty(doctorpassword)) {
-            if (validationError) {
-                validationErrorMessage.append("  ");
-            }
-            validationError = true;
-            validationErrorMessage.append("password");
-        }
-        if (isEmpty(doctorconformpassword)) {
-            if (validationError) {
-                validationErrorMessage.append("  ");
-            }
-            validationError = true;
-            validationErrorMessage.append(" ");
-        } else {
-            if (!isMatching(doctorpassword, doctorconformpassword)) {
-                if (validationError) {
-                    validationErrorMessage.append(" and ");
-                }
-                validationError = true;
-                validationErrorMessage.append("confirm password again");
-            }
-        }
-        validationErrorMessage.append(".");
-
-        if (validationError) {
-            Toast.makeText(DoctorSignupActivity.this, validationErrorMessage.toString(), Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        //Setting up a progress dialog
-        final ProgressDialog dlg = new ProgressDialog(DoctorSignupActivity.this);
-        dlg.setTitle("Please, wait a moment.");
-        dlg.setMessage("Signing up...");
-        dlg.show();
-
-        ParseUser user = new ParseUser();
-        user.setUsername(doctorusername.getText().toString());
-        user.setPassword(doctorpassword.getText().toString());
-        user.signUpInBackground(new SignUpCallback() {
             @Override
-            public void done(ParseException e) {
-                if (e == null) {
-                    dlg.dismiss();
-                    alertDisplayer("Sucessful Login", "Welcome " + doctorusername.getText().toString() + "!");
+            public void onClick (View v){
+                //Validating the log in data
+                boolean validationError = false;
 
-                } else {
-                    dlg.dismiss();
-                    ParseUser.logOut();
-                    Toast.makeText(DoctorSignupActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                StringBuilder validationErrorMessage = new StringBuilder("Please, insert ");
+                if (isEmpty(doctorusername)) {
+                    validationError = true;
+                    validationErrorMessage.append("username");
                 }
+                if (isEmpty(departmentname)) {
+                    if (validationError) {
+                        validationErrorMessage.append("department name");
+                    }
+                    validationError = true;
+                    validationErrorMessage.append(" ");
+                }
+                if (isEmpty(doctormailid)) {
+                    if (validationError) {
+                        validationErrorMessage.append("doctor mailid");
+                    }
+                    validationError = true;
+                    validationErrorMessage.append(" ");
+                }
+                if (isEmpty(doctorphno)) {
+                    if (validationError) {
+                        validationErrorMessage.append("doctor phonenumber");
+                    }
+                    validationError = true;
+                    validationErrorMessage.append(" ");
+                }
+                if (isEmpty(doctorpassword)) {
+                    if (validationError) {
+                        validationErrorMessage.append("  ");
+                    }
+                    validationError = true;
+                    validationErrorMessage.append("password");
+                }
+                if (isEmpty(doctorconformpassword)) {
+                    if (validationError) {
+                        validationErrorMessage.append("  ");
+                    }
+                    validationError = true;
+                    validationErrorMessage.append(" ");
+                } else {
+                    if (!isMatching(doctorpassword, doctorconformpassword)) {
+                        if (validationError) {
+                            validationErrorMessage.append(" and ");
+                        }
+                        validationError = true;
+                        validationErrorMessage.append("confirm password again");
+                    }
+                }
+                validationErrorMessage.append(".");
+
+                if (validationError) {
+                    Toast.makeText(DoctorSignupActivity.this, validationErrorMessage.toString(), Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                //Setting up a progress dialog
+                final ProgressDialog dlg = new ProgressDialog(DoctorSignupActivity.this);
+                dlg.setTitle("Please, wait a moment.");
+                dlg.setMessage("Signing up...");
+                dlg.show();
+
+                ParseUser user = new ParseUser();
+                user.setUsername(doctorusername.getText().toString());
+                user.setPassword(doctorpassword.getText().toString());
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            dlg.dismiss();
+                            alertDisplayer("Sucessful Login", "Welcome " + doctorusername.getText().toString() + "!");
+
+                        } else {
+                            dlg.dismiss();
+                            ParseUser.logOut();
+                            Toast.makeText(DoctorSignupActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
             }
+
         });
     }
-
-    });
-}
 
     private boolean isEmpty(EditText text) {
         if (text.getText().toString().trim().length() > 0) {
@@ -255,8 +222,4 @@ public class DoctorSignupActivity extends AppCompatActivity {
         }
         }
  */
-
-
-
-
 
