1ade16940ed1da66fe34ce6c3ef14c97674f1f77
vamshiredd <vamshikrishnay09>
2019-12-02 21:13:33 -0600

    committed some files with changes that all required as per our application

--- app/src/main/java/com/example/androidproject/HistoryloginActivity.java
+++ app/src/main/java/com/example/androidproject/HistoryloginActivity.java
@@ -13,9 +13,7 @@ public class HistoryloginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historylogin);
     }
-    public static final int patient1 =  0;
-    public static final int patient2 =  0;
-    public static final int patient3 =  0;
+
     public static final int healthlogin = 0;
     public static final int back6 = 0;
 
@@ -31,20 +29,7 @@ public class HistoryloginActivity extends AppCompatActivity {
       //  startActivityForResult(intent,1);
    // }
 
-    public void patient1(View v){
-        Intent intent = new Intent(this, Patient1Activity.class);
-        startActivityForResult(intent,patient1);
-    }
-    public void patient2(View v){
-        Intent intent = new Intent(this, Patient2Activity.class);
-        startActivityForResult(intent,patient2);
 
-    }
-    public void patient3(View v){
-        Intent intent = new Intent(this, Patient3Activity.class);
-        startActivityForResult(intent,patient3);
-
-    }
     public void back6Click(View v) {
         Intent intent = new Intent(this, DoctorHomeActivity.class);
         startActivityForResult(intent,back6);
